46b675a4e5a29d94de3c971af6ff0da6c6e6b646
akeemseymens <akeem.seymens@holbertonschool.com>
2019-09-24 20:55:23 +0000

    added header

--- monty.h
+++ monty.h
@@ -1,3 +1,6 @@
+#ifndefine _MONTY_H
+#defint _MONTY_H
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -26,3 +29,4 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+#endif
