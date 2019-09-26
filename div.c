be29957e85d10d57292bf9fd570ddeff2592babf
akeemseymens <akeem.seymens@holbertonschool.com>
2019-09-26 20:10:35 +0000

    added div

---
+++ div.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * div - Diide the second element of the stack by the top element
+ * @head: The stack
+ * @line_number: The line number
+ */
+void div(stack_t **head, unsigned int line_number)
+{
+	stack_t *p;
+
+	if (!(*head && (*head)->next))
+	{
+		dprintf(STDERR_FILENO,
+			"L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else if ((*head)->n == 0)
+	{
+		dprintf(STDERR_FILENO,
+			"L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		(*head)->next->n /= (*head)->n;
+		p = *head;
+		*head = (*head)->next;
+		(*head)->prev = NULL;
+		free(p);
+	}
+}
