b9966aa3916fc64a66a2f665e6b74a042cde84ee
Ben Cook <808@holbertonschool.com>
2019-09-26 10:53:29 -0700

    update push to work with negative integers

--- push.c
+++ push.c
@@ -20,10 +20,20 @@ void push(stack_t **head, unsigned int n)
 		exit(EXIT_FAILURE);
 	}
 
-	for (i = 0; global.value[i] != '\0'; i++)
+	i = 0;
+	if (global.value[0] == '\0')
+	{
+		dprintf(STDERR_FILENO, "L%u usage: push integer\n", n);
+		free(new);
+		exit(EXIT_FAILURE);
+	}
+	else if (global.value[0] == '-' && global.value[1] != '\0')
+		i++;
+	for (; global.value[i] != '\0'; i++)
 		if (!isdigit(global.value[i]))
 		{
 			dprintf(STDERR_FILENO, "L%u usage: push integer\n", n);
+			free(new);
 			exit(EXIT_FAILURE);
 		}
 	new->n = atoi(global.value);
